a4349af0a03ac1da05faec3112f4cd4fc1e63677
Alexander Johnson <work.a.a.johnson@gmail.com>
2023-06-24 17:46:24 -0500

    implements new buy command

--- src/ui/Command.java
+++ src/ui/Command.java
@@ -43,7 +43,7 @@ public class Command {
                         System.out.printf("%d.\t%s\n", i, this.hand.get(i - 1));
                     }
                 }
-                System.out.printf("\nNum books: %d\nNum runs: %d\n", this.numBooks, this.numRuns);
+                System.out.printf("\nNum books:\t%d\nNum runs:\t%d\n", this.numBooks, this.numRuns);
                 break;
             case "books":
                 setNumBooks(line);
@@ -59,6 +59,9 @@ public class Command {
                 break;
             case "new":
                 createNew(line);
+                break;
+            case "buy":
+                buy(line);
                 break;
             case "build":
                 buildBases(line);
@@ -89,11 +92,12 @@ public class Command {
         help.put("score", "determine your current score");
         help.put("books [num books]", "set the number of books");
         help.put("runs [num runs]", "set the number of runs");
-        help.put("decks [num]", "specify number of decks to draw cards from (2 by default)");
-        help.put("add [card name] [num]", "add new card(s) to hand (1 by default)");
-        help.put("new [num]", "instantiates a new hand of randomly drawn cards\n\t\t(11 by default)");
+        help.put("decks [num decks]", "specify number of decks to draw cards from\n\t\t(2 by default)");
+        help.put("add [card name] [num cards]", "add new card(s) of given name to hand\n\t\t(1 by default)");
+        help.put("new [num cards]", "instantiates a new hand of randomly drawn cards\n\t\t(11 by default)");
+        help.put("buy [card name]", "determine whether a given card should be bought");
         help.put("build", "determine the best bases for building books and/or runs,\n\t\tand how many more cards are needed.");
-        help.put("sort [num]", "sort the hand of cards by:\n\t\t\t0 - number\n\t\t\t1 - suit");
+        help.put("sort [option]", "sort the hand of cards by:\n\t\t\t0 - number\n\t\t\t1 - suit");
         help.put("reset", "resets the player's hand");
         help.put("exit", "terminates the program");
         System.out.println("List of valid commands:");
@@ -207,6 +211,39 @@ public class Command {
         }
     }
 
+    public void buy(String input) {
+        String[] tokens = input.split(" ");
+        if (tokens.length == 1) {
+            System.out.println("Error: expecting at least one argument");
+        } else {
+            // At least one argument
+            // Parse card name
+            try {
+                Card card = Card.fromString(tokens[1]);
+                List<Card> handCopy = new ArrayList<>(this.hand);
+                Map<String, List<List<Card>>> bases = Algorithm.getOptimalBases(
+                        handCopy, this.numBooks, this.numRuns);
+                // Calculate how many cards are missing before adding card
+                int initMissingCards = Algorithm.numCardsMissing(
+                        bases, this.numBooks, this.numRuns);
+                handCopy.add(card);
+                bases = Algorithm.getOptimalBases(
+                        handCopy, this.numBooks, this.numRuns);
+                // Calculate how many cards are missing after adding card
+                int newMissingCards = Algorithm.numCardsMissing(
+                        bases, this.numBooks, this.numRuns);
+                if (newMissingCards < initMissingCards) {
+                    // If the number of needed cards are lower, then buy
+                    System.out.println("yes");
+                } else {
+                    System.out.println("no");
+                }
+            } catch (BadEncodingException e) {
+                System.out.printf(INVALID_CARD_MSG, tokens[1]);
+            }
+        }
+    }
+
     public void buildBases(String input) {
         String[] tokens = input.split(" ");
         Map<String, List<List<Card>>> bases = Algorithm.getOptimalBases(
@@ -219,7 +256,7 @@ public class Command {
             for (List<Card> stack : entry.getValue()) {
                 // Print out bases
                 numPrinted++;
-                System.out.printf("%s %d: %s\n", label, numPrinted, stack);
+                System.out.printf("%s %d:\t%s\n", label, numPrinted, stack);
             }
         }
         int numCardsMissing = Algorithm.numCardsMissing(bases, this.numBooks, this.numRuns);
@@ -231,6 +268,8 @@ public class Command {
         if (tokens.length == 1) {
             System.out.println("Error: expecting at least one argument");
         } else {
+            // At least one argument
+            // Parse option
             int num = parseInt(tokens[1]);
             if (num != INVALID_INT) {
                 switch(num) {
